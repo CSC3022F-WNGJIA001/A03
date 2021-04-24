b3688067c9e01eb73985bbb8dd6a11206637ea53
Jialong Weng <wngjia001@myuct.ac.za>
2021-04-25 00:14:32 +0200

    Add driver source file to control classes and functions

---
+++ src/driver.cpp
@@ -0,0 +1,60 @@
+/**
+* driver.cpp containing main function
+* CSC3022F Assignment 3 2021
+* Author: WNGJIA001
+*/
+
+#include <fstream>  
+#include <iostream> 
+#include <sstream>  
+#include <string>
+#include <vector>
+
+#include "cmdlineParser.h"
+#include "ConnectedComponent.h"
+#include "PGMimageProcessor.h"
+
+int main(int argc, char *argv[]) {
+    using namespace WNGJIA001;
+    // call function to parse command line options
+    parseCmdline(argc, argv);
+    
+    // instantiate image processor class
+    PGMimageProcessor myProcessor(in_PGM_file);
+
+    // assign default values if not passed as command line options 
+    if ((min_size == -1) && (max_size == -1)) { // no -s flag parsed: default min = 3; max = img_width * img_height
+        min_size = 3;
+        max_size = myProcessor.getWidth()*myProcessor.getHeight();
+    }
+    if (threshold == -1) { // no -t flag parsed: default threshold = 128
+        threshold = 128;
+    }
+
+    // extract components
+    std::cout << std::endl;
+    int cc_num = myProcessor.extractComponents((unsigned char)threshold, min_size);
+    std::cout << "Connected component extraction completed: " << cc_num << " components extracted" << std::endl;
+    // clear up memory used to hold the PGM input image
+    myProcessor.clearMemory();
+
+    // filter
+    if ((min_size != -1) && (max_size != -1)) { // custom min and max sizes: do filter
+        std::cout << std::endl;
+        int i = myProcessor.filterComponentsBySize(min_size, max_size);
+        std::cout << "Filtering completed: " << i << " components remaining" << std::endl;
+    }
+    
+    // print
+    if (p_flag) {
+        std::cout << std::endl;
+        myProcessor.printData();
+    }
+
+    // write out
+    std::cout << std::endl;
+    if (myProcessor.writeComponents(out_PGM_file)) {
+        std::cout << "Output PGM image has been successful generated as bin/" << out_PGM_file << "\n" << std::endl;
+    }
+    return 0;
+}
